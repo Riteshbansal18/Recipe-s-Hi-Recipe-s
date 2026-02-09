bf31391b7cf1586c07ee758830c315b1629c8484
Manpreet Kaur <manpreetkaur81008@gmail.com>
2026-02-09 19:57:48 +0530

    frontend changes

--- frontend/src/pages/AddRecipeModal.jsx
+++ frontend/src/pages/AddRecipeModal.jsx
@@ -14,6 +14,7 @@ import {
 } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 
+// testing first
 export const AddRecipeModal = () => {
   const isAuth = useSelector((store) => store.authReducer.isAuth);
   const [isOpen, setIsOpen] = useState(false);
