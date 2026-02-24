c10ac11f85de112865d18f70a94a854225ae27c8
Manpreet Kaur <manpreetkaur81008@gmail.com>
2026-02-24 12:22:41 +0530

    Fix: Feed page blank on reload - improved PrivateRoute and auto-load user data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { io } from "socket.io-client";
+import { useToast } from "@chakra-ui/react";
 
 import { AllRoutes } from "./routes/AllRoutes";
 import { Navbar } from "./components/common/Navbar";
 import { AddRecipeModal } from "./pages/AddRecipeModal";
 import Footer from "./components/Footer";
 import { SOCKET_NEW_RECIPE } from "./redux/recipeReducer/actionTypes";
+import { getUserData } from "./redux/authReducer/actions";
 
 // 🔥 SOCKET INSTANCE
 const socket = io(process.env.REACT_APP_API_URL);
 
 function App() {
   const dispatch = useDispatch();
+  const toast = useToast();
+  const token = useSelector((store) => store.authReducer.token) || localStorage.getItem("token");
+  const loggedInUser = useSelector((store) => store.authReducer.loggedInUser);
+
+  // 🔥 FIX: Load user data on app mount if token exists
+  useEffect(() => {
+    if (token && !loggedInUser) {
+      dispatch(getUserData(token, toast));
+    }
+  }, [token, loggedInUser, dispatch, toast]);
 
   useEffect(() => {
     // listen for real-time recipe
--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -1,11 +1,15 @@
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
   const isAuth = useSelector((store) => store.authReducer.isAuth);
+  const token = useSelector((store) => store.authReducer.token);
   const location = useLocation();
 
-  if (!isAuth) {
+  // 🔥 FIX: Check both Redux state AND localStorage
+  const hasToken = token || localStorage.getItem("token");
+
+  if (!isAuth && !hasToken) {
     return (
       <Navigate
         to="/login"
